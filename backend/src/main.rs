861089e90a2b64f62b23666787fc4b77c9988513
itoooak <101097771+itoooak@users.noreply.github.com>
2023-09-08 20:16:42 +0900

    データの持ち方を`Vec`から`HashMap`に変更

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,51 +2,73 @@ use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::{
+    collections::HashMap,
     fs::{self, File},
     io::Read,
     sync::{Arc, RwLock},
 };
 
+type ID = String;
+type Index = HashMap<ID, BookData>;
+
 const DATA_PATH: &str = "../data/data.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 struct BookData {
-    id: String,
     title: String,
 }
 
-fn init_data() -> Vec<BookData> {
+fn init_data() -> Index {
     if let Ok(mut file) = File::open(DATA_PATH) {
         let mut serialized = String::new();
         file.read_to_string(&mut serialized)
             .expect("failed to read data from file");
-        let data: Vec<BookData> = serde_json::from_str(&serialized).expect("cannot parse file");
+        let data: Index = serde_json::from_str(&serialized).expect("cannot parse file");
         data
     } else {
-        Vec::<BookData>::new()
+        Index::new()
     }
 }
 
-async fn get_item_list(list: State<Arc<RwLock<Vec<BookData>>>>) -> impl IntoResponse {
+async fn get_item_list(list: State<Arc<RwLock<Index>>>) -> impl IntoResponse {
     let items = list.read().unwrap();
     let content = serde_json::to_vec_pretty(&*items).unwrap();
     fs::write(DATA_PATH, content).expect("failed to save data to file");
     Json(json!(*items))
 }
 
-async fn create_item(
-    list: State<Arc<RwLock<Vec<BookData>>>>,
-    Json(payload): Json<BookData>,
+#[derive(Serialize, Deserialize)]
+struct UpsertRequest {
+    id: ID,
+    data: BookData,
+}
+
+async fn upsert_item(
+    list: State<Arc<RwLock<Index>>>,
+    Json(request): Json<UpsertRequest>,
 ) -> impl IntoResponse {
-    list.write().unwrap().push(payload);
-    Json(json!({ "created": list.read().unwrap().last() }))
+    match (*list)
+        .write()
+        .unwrap()
+        .insert(request.id.clone(), request.data.clone())
+    {
+        Some(old) => Json(json!({
+            "updated": request.id,
+            "old": old,
+            "new": request.data
+        })),
+        None => Json(json!({
+            "created": request.id,
+            "data": request.data
+        })),
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/books", get(get_item_list).post(create_item))
+        .route("/books", get(get_item_list).post(upsert_item))
         .with_state(Arc::new(RwLock::new(init_data())));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
