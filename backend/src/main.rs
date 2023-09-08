6d11bd073ee90123ddca704735aa1388abd43b74
itoooak <101097771+itoooak@users.noreply.github.com>
2023-09-08 19:32:57 +0900

    項目追加時にデータをファイルに書き出し, 起動時にファイルからデータを読み出し

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,13 @@
 use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::sync::{Arc, RwLock};
+use std::{
+    fs::{self, File},
+    io::Read,
+    sync::{Arc, RwLock},
+};
+
+const DATA_PATH: &str = "../data/data.json";
 
 #[derive(Serialize, Deserialize)]
 struct BookData {
@@ -9,8 +15,23 @@ struct BookData {
     title: String,
 }
 
+fn init_data() -> Vec<BookData> {
+    if let Ok(mut file) = File::open(DATA_PATH) {
+        let mut serialized = String::new();
+        file.read_to_string(&mut serialized)
+            .expect("failed to read data from file");
+        let data: Vec<BookData> = serde_json::from_str(&serialized).expect("cannot parse file");
+        data
+    } else {
+        Vec::<BookData>::new()
+    }
+}
+
 async fn get_item_list(list: State<Arc<RwLock<Vec<BookData>>>>) -> impl IntoResponse {
-    Json(json!(*list.read().unwrap()))
+    let items = list.read().unwrap();
+    let content = serde_json::to_vec_pretty(&*items).unwrap();
+    fs::write(DATA_PATH, content).expect("failed to save data to file");
+    Json(json!(*items))
 }
 
 async fn create_item(
@@ -26,7 +47,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/books", get(get_item_list).post(create_item))
-        .with_state(Arc::new(RwLock::new(Vec::<BookData>::new())));
+        .with_state(Arc::new(RwLock::new(init_data())));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
