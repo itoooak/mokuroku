9949445c984abe9efc8126b23f391ee0d2bcae0e
itoooak <101097771+itoooak@users.noreply.github.com>
2024-06-06 11:06:40 +0900

    `lib.rs`を削除

--- backend/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod db;
-pub mod handler;
-
-#[derive(Debug, Clone, PartialEq, sqlx::FromRow, serde::Serialize, serde::Deserialize)]
-pub struct Book {
-    pub id: String,
-    pub title: String,
-}
